00c3d0a6a5e101eb9feb5652c1d6ceffdcf1f708
titofranco <titofranco@gmail.com>
2011-03-22 12:38:58 -0500

    Simple implementation of GHeat

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,46 +1,66 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 
-function windowHeight() {
-    //Standard browsers (Mozilla,Safari,etc.)
-    if (self.innerHeight) return self.innerHeight;
-    // IE 6
-    if (document.documentElement && document.documentElement.clientHeight) return y = document.documentElement.clientHeight;
-    //IE 5
-    if (document.body) return document.body.clientHeight;
-    //Just in case
-    return 0;
-}
-
-/* The offsetHeight and offsetWidth properties are provided by the browser,
-   and return—in pixels—the dimensions of their element, including any padding.*/
-/* Redimensiona el tamño del mapa y de la barra lateral*/
-
-function handleResize() {
-    var height = windowHeight() - document.getElementById('toolbar').offsetHeight - 45;
-    document.getElementById('map').style.height = height + 'px';
-    document.getElementById('sidebar').style.height = (height - 18) + 'px';
-}
-
-
+var map;
+setTimeout('init()', 1);
 
 function init(){
 
    if (GBrowserIsCompatible()){
-       var map;
-       var centerLatitude = 6.27488;
-       var centerLongitude = -75.56817;
+       var centerLatitude = 38.5;
+       var centerLongitude = -121.8;
        var startZoom = 15;
        
        map = new GMap2(document.getElementById("map"));
-       map.setCenter(new GLatLng(centerLatitude,centerLatitude),startZoom);
+       map.setCenter(new GLatLng(centerLatitude,centerLongitude),startZoom);
        map.setMapType(G_HYBRID_MAP);
        map.setUIToDefault();
+       initHeatmap();
         
+   }else{
+    alert("Your browser is not compatible")
    }
+}   
 
+
+
+ 
+function initHeatmap()
+{
+ // Heatmap Scripts
+ try
+ {
+  var myHM = new GEOHeatmap();
+  myHM.Init(400, 900); // Must be same as map size, or same ratio
+
+  var data = new Array();
+
+  // Generate random lat/lon and value points
+
+  for(p=0;p<50;p++)
+  {
+   var rLatD=Math.floor(Math.random()*1000);
+   var rLonD=Math.floor(Math.random()*1000);
+   var rValD=Math.floor(Math.random()*10);
+   data.push(38.47+(rLatD/15000));
+   data.push(-121.84+(rLonD/15000));
+   data.push(rValD);
+  }
+
+  myHM.SetData(data);
+  myHM.SetBoost(1); // Optional, see documentation
+  myHM.SetDecay(0); // Optional, see documentation
+  var preUrl = myHM.GetURL();
+
+  // Now render in our Google Map
+  var heatmapOverlay = new HMGoogleOverlay(preUrl);
+  map.addOverlay(heatmapOverlay);
+ }
+ catch(e)
+ {
+  alert(e.Message);
+ }
 }
-//window.onresize = handleResize;
+
 window.onload = init;
 window.onunload = GUnload;
-
